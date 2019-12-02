207e0350b97260384e2408ae3c31cfb6a57e72b2
brayooh <brianndichu.bn@gmail.com>
2019-12-02 15:59:17 +0300

    add profile function + add class method to create bussiness

--- neighboor/views.py
+++ neighboor/views.py
@@ -84,4 +84,99 @@ def updateprofile(request):
         user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.profile)
 
-   
+    context = {
+    'user_form':user_form,
+    'profile_form':profile_form,
+    }
+
+    return render(request, 'updateprofile.html', context)
+
+
+@login_required
+
+def profile(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been successfully updated')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+    'user_form':user_form,
+    'profile_form':profile_form,
+    }
+    return render(request, 'profile.html', context)
+
+def hood(request):
+    profile = Profile.objects.all()
+    context = {
+        "profile":profile,
+    }
+    return render(request, 'navbar.html', context)
+
+@login_required
+def poststory(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = StoryForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = current_user
+            project.save()
+        return redirect('/')
+    else:
+        form = StoryForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'create-story.html', context)
+
+def get_project(request, id):
+    project = Projects.objects.get(pk=id)
+
+    return render(request, 'project.html', {'project':project})
+
+
+
+@login_required
+def createhood(request):
+    current_user = request.user
+    if request.method == 'POST':
+        hood_form = NeighborhoodForm(request.POST, request.FILES)
+        if hood_form.is_valid():
+            post = hood_form.save(commit=False)
+            post.user = current_user
+            post.save()
+        return redirect('/')
+    else:
+        hood_form = NeighborhoodForm()
+    context = {
+        "hood_form":hood_form,
+    }
+    return render(request, 'hood.html', context)
+
+
+
+
+@login_required
+def createbusiness(request):
+    current_user = request.user
+    if request.method == 'POST':
+        business_form = BusinessForm(request.POST, request.FILES)
+        if business_form.is_valid():
+            post = business_form.save(commit=False)
+            post.user = current_user
+            post.save()
+        return redirect('/')
+    else:
+        business_form = BusinessForm()
+    context = {
+        "business_form":business_form,
+    }
+    return render(request, 'business.html', context)
