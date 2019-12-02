d9084dd4e6e0407923f6940ca8f9d7793d3d9970
brayooh <brianndichu.bn@gmail.com>
2019-12-02 16:15:59 +0300

    add save profile instance to class profile

--- neighboor/models.py
+++ neighboor/models.py
@@ -40,4 +40,13 @@ class Profile(models.Model):
     class Meta:
         db_table ='Profile'
 
- 
+    @receiver(post_save, sender=User)
+    def update_create_profile(sender,instance,created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save,sender = User)
+    def save_profile(sender,instance,**kwargs):
+        instance.profile.save()
+
+
