5ffe2fd0ef04480c8e52f6a795cd311c016e5278
brayooh <brianndichu.bn@gmail.com>
2019-12-02 15:16:39 +0300

    create views + add home function

---
+++ neighboor/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import *
+from .models import *
+from .emails import send_welcome_email
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+@login_required
+def home(request):
+    posts = Post.objects.all()
+    hoods = Neighborhood.objects.all()
+    businesses = Business.objects.all()
+    context = {
+        "posts":posts,
+        "hoods":hoods,
+        "businesses":businesses,
+    }
+    return render(request, 'index.html', context)
+
