75350dd3a2c6d6da5d7fedd56ef281118372a273
brayooh <brianndichu.bn@gmail.com>
2019-12-02 15:24:23 +0300

    modify home function + add registration function

--- neighboor/views.py
+++ neighboor/views.py
@@ -13,6 +13,59 @@ def home(request):
     posts = Post.objects.all()
     hoods = Neighborhood.objects.all()
     businesses = Business.objects.all()
- 
+    context = {
+        "posts":posts,
+        "hoods":hoods,
+        "businesses":businesses,
+    }
     return render(request, 'index.html', context)
+
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+
+            username=form.cleaned_data['username']
+            email=form.cleaned_data['email']
+            password1=form.cleaned_data['password1']
+            recipient=User(username=username,email=email)
+            try:
+                send_welcome_email(username,email)
+                messages.success(request, f'Account has been created successfully!')
+            except:
+                print('error')
+            return redirect('/login')
+    else:
+        form = RegisterForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'users/register.html', context)
+
+
+
+@login_required
+def search_business(request):
+    businesses = Business.objects.all()
+    if 'business' in request.GET and request.GET['business']:
+        search_term = request.GET["business"]
+        searched_business = Business.search_by_name(search_term)
+        print('*********',searched_business)
+        message = f'{search_term}'
+        context = {
+            "searched_business":searched_business,
+            "message":message,
+            "businesses":businesses,
+
+        }
+        return render(request, 'search.html', context)
+    else:
+        message = "You haven't searched for any user"
+        context = {
+            "message":message,
+        }
+        return render(request, 'search.html', context)
+
 
